f62882e60e6528b930d84127f42adf3d0fafa0b7
Jon Hadfield <jon@lessknown.co.uk>
2021-10-05 19:01:57 +0100

    fix subscription id stripping.

--- set.go
+++ set.go
@@ -124,9 +124,9 @@ func setPackerImageGalleryDestinationImageVersion(s *session, path string, i Set
 	}
 
 	// we need to remove the builder's subscription id to prevent interactive oauth authentication
-	if i.Unattended && jt.Builders[0].SubscriptionID != "" {
+	if i.Unattended && builder.SubscriptionID != "" {
 		fmt.Println("stripping subscription_id from builder to allow for unattended (no oauth) build")
-		jt.Builders[0].SubscriptionID = ""
+		builder.SubscriptionID = ""
 	}
 
 	builder.SharedGalleryDestination.SigDestinationImageVersion = newVer.String()
